de2030291c340023156aa19eb20bf9f6dbcb938c
mac <mac@macs-MacBook-Pro.local>
2021-12-30 19:50:35 +0500

    Added material table instead of list alse added fetch api

--- src/app/TodoApp.js
+++ src/app/TodoApp.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import UpdateIcon from '@mui/icons-material/Update';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import TodoList from './views/TodoList';
@@ -10,25 +11,35 @@ class TodoApp extends React.Component {
 
     constructor(props) {
       super(props);
-      this.state = { items: [], text: '' };
+      this.state = { items: [], text: '', updatingItem: null };
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
       this.handleItemDelete = this.handleItemDelete.bind(this);
+      this.handleItemEdit = this.handleItemEdit.bind(this);
       this.deleteAll = this.deleteAll.bind(this);
+      this.fetchItems = this.fetchItems.bind(this);
+    }
+
+    componentDidMount() {
+
+      this.fetchItems();
+
     }
   
     render() {
-
       return (
         <Box sx={{ '& > :not(style)': { m: 1 } }}>
             <TextField id="new-todo" onChange={this.handleChange} size="small" label="What needs to be done?" variant="outlined" value={this.state.text} />
-            <Button variant="outlined" onClick={this.handleSubmit} startIcon={<AddIcon />}>
+            {!this.state.updatingItem && <Button variant="outlined" onClick={this.handleSubmit} startIcon={<AddIcon />}>
                 Add
-            </Button>
-            <TodoList items={this.state.items} onDelete={this.handleItemDelete} />
-            <Button variant="outlined" onClick={(e) => this.deleteAll(e)} startIcon={<DeleteIcon />}>
+            </Button>}
+             {this.state.updatingItem && <Button variant="outlined" onClick={this.handleSubmit} startIcon={<UpdateIcon />}>
+                Update
+            </Button>}
+            <TodoList items={this.state.items} onDelete={this.handleItemDelete} onEdit={this.handleItemEdit} />
+            {this.state.items.length && <Button variant="outlined" onClick={(e) => this.deleteAll(e)} startIcon={<DeleteIcon />}>
               Delete Tasks
-            </Button>
+            </Button>}
         </Box>
       );
     }
@@ -38,38 +49,99 @@ class TodoApp extends React.Component {
     }
   
     handleSubmit(e) {
-      e.preventDefault();
       if (this.state.text.length === 0) {
         return;
       }
-      const newItem = {
-        text: this.state.text,
-        id: ( new Date( Date.now() ) ).getMilliseconds()
-      };
-      this.setState(state => ({
-        items: state.items.concat(newItem),
-        text: ''
-      }));
+
+      if (!this.state.updatingItem) {
+        const newItem = {
+          text: this.state.text,
+          id: Date.now()
+        };
+        this.setState(state => ({
+          items: state.items.concat(newItem),
+          text: ''
+        }));
+      } else {
+
+
+        const items = this.state.items;
+        const itemIndex = this.state.items.indexOf(this.state.updatingItem);
+        items[itemIndex] = {
+          text: this.state.text,
+          id: this.state.updatingItem.id
+        };
+        this.setState(state => ({
+          items,
+          text: '',
+          updatingItem: null
+        }));
+
+      }
     }
 
     handleItemDelete(item) {
 
-        const items = this.state.items;
-        const itemIndex = this.state.items.indexOf(item);
-        items.splice(itemIndex, 1);
-        this.setState(state => ({
-            items
-          }));
+      if (this.state.updatingItem === item) {
+        return;
+      }
+      const items = this.state.items;
+      const itemIndex = this.state.items.indexOf(item);
+      items.splice(itemIndex, 1);
+      this.setState(state => ({
+          items
+      }));
         
+    }
+
+    handleItemEdit(item) {
+
+      this.setState(state => ({
+          text: item.text,
+          updatingItem: item
+        }));
+      
     }
 
     deleteAll(e) {
 
       this.setState({
-        items: []
+        items: [],
+        updatingItem: null
       });
-      
-  }
+    }
+
+    async fetchItems () {
+
+      try {
+
+        const itemsUrl = `https://my-json-server.typicode.com/OsamaShahid/dev/items`;
+        const itemsHeaders = new Headers({
+          'Content-Type': 'application/json'
+        });
+        const itemsRequest = new Request(itemsUrl, {
+          method: 'GET',
+          headers: itemsHeaders,
+          mode: 'cors',
+          cache: 'default',
+        });
+
+        let itemsRes = await fetch(itemsRequest);
+
+        if (itemsRes.ok) {
+          itemsRes = await itemsRes.json();
+          this.setState(state => ({
+            items: itemsRes
+          }));
+        }
+
+      } catch (error) {
+
+        console.log(error);
+
+      }
+
+    }
 
   }
   
---
+++ src/app/views/TodoList/TodoList.jsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+
+class TodoList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.hadleEdit = this.handleEdit.bind(this);
+    }
+
+    handleDelete(item, e) {
+        this.props.onDelete(item);
+    }
+
+    handleEdit(item, e) {
+      this.props.onEdit(item);
+  }
+
+    render() {
+      return (
+        <Box>
+          {this.props.items.length > 0 && <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Id</TableCell>
+                  <TableCell align="right">Text</TableCell>
+                  <TableCell align="right">Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.props.items.map((item) => (
+                  <TableRow
+                    key={`${item.id}-${item.text}`}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {item.id}
+                    </TableCell>
+                    <TableCell align="right">{item.text}</TableCell>
+                    <TableCell align="right">
+                      <Tooltip title="Delete">
+                          <IconButton edge="end" onClick={(e) => this.handleDelete(item, e)} aria-label="delete">
+                              <DeleteIcon />
+                          </IconButton>
+                      </Tooltip>
+                      <Tooltip title="Edit">
+                          <IconButton edge="end" onClick={(e) => this.handleEdit(item, e)} aria-label="edit">
+                              <EditIcon />
+                          </IconButton>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>}
+        </Box>
+      );
+    }
+  }
+
+
+export default TodoList;
